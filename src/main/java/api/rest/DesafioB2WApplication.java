25fcfe425da1281b6034f4d33b32430c54d751fe
Luiz Otavio <luizotaviomartinsf@gmail.com>
2018-12-04 19:32:24 -0200

    Versão Incial do Projeto

---
+++ src/main/java/api/rest/DesafioB2WApplication.java
@@ -0,0 +1,12 @@
+package api.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioB2WApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioB2WApplication.class, args);
+	}
+}
